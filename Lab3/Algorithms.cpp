fa6be9fc575098ab63d6b285a7b4e7d702b1ae6b
Grodster1 <wiktorwszedybyl1@gmail.com>
2026-05-23 22:50:29 +0200

    Added dynamic programming

--- Lab3/Algorithms.cpp
+++ Lab3/Algorithms.cpp
@@ -35,3 +35,45 @@ Solution lpt(const Instance & inst){
     return sol;
 }
 
+Solution dynamicProgP2(const Instance & inst){
+    int sumP = inst.totalP();
+    int K = sumP / 2 + 1;
+    int n = inst.size();
+
+    Solution sol(inst.m);
+    sol.assignment.resize(n, -1);
+
+    std::vector<std::vector<int>> T(n+1, std::vector<int>(K,0));
+    T[0][0]=1;
+    for(int j = 1; j <= n; ++j){
+        int pj = inst.jobs[j-1].p;
+        for(int k = 0; k < K; ++k){
+            if(T[j-1][k] == 1){
+                T[j][k] = 1;  
+            }
+            if (k >= pj && T[j-1][k - pj] == 1) {
+                T[j][k] = 1;  
+            }
+        }
+    }
+
+    int bestK = 0;
+    for(int k =0; k < K; ++k){
+        if(T[n][k]==1) bestK =k;
+    }
+
+    int k = bestK;
+    for (int j = n; j >= 1; --j) {
+        if (T[j-1][k] == 1) {
+            sol.assignment[j-1] = 1;
+        } else {
+            sol.assignment[j-1] = 0;
+            k -= inst.jobs[j-1].p;
+        }
+    }
+    sol.computeCMax(inst.jobs);
+
+    return sol;
+
+}
+
